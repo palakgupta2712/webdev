5b6f75fe4ee8f3100c4221ad2f3b85b86e4f34de
palakgupta2712 <61227144+palakgupta2712@users.noreply.github.com>
2020-07-14 16:55:39 +0530

    Jersey Services

---
+++ services/account/src/main/java/mybank/mainAccount.java
@@ -0,0 +1,34 @@
+package mybank;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class mainAccount {
+    public static final String BASE_URI = "http://localhost:8082/mybank/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("mybank");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+
+        server.getServerConfiguration().addHttpHandler(new StaticHttpHandler("D:\\Downloads\\soa_project-1.1\\website\\"), "/");
+        System.out.println(String.format(
+                "Jersey app started with WADL available at " + "%sapplication.wadl\nHit enter to stop it...",
+                BASE_URI));
+        // Open the URI in Chrome (Windows)
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8082/mybank.html");
+
+        System.in.read();
+        server.shutdownNow();
+    }
+}
